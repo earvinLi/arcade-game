d88f2055ae878e2dd6b7e83352fbccebf5fb1a41
Earvin Li <earvin.tli@gmail.com>
2017-11-25 20:45:48 -0800

    add function to cut the enemy that is out of screen inside checkCollision function

--- js/engine.js
+++ js/engine.js
@@ -9,7 +9,7 @@
  * drawn but that is not the case. What's really happening is the entire "scene"
  * is being drawn over and over, presenting the illusion of animation.
  *
- * This engine makes the canvas' context (ctx) object globally available to make 
+ * This engine makes the canvas' context (ctx) object globally available to make
  * writing app.js a little simpler to work with.
  */
 
@@ -77,9 +77,19 @@ var Engine = (function(global) {
      * functionality this way (you could just implement collision detection
      * on the entities themselves within your app.js file).
      */
+
+     function checkCollisions() {
+         $.each(allEnemies, function(enemy) {
+             if (enemy) {
+                 if (v.x > 505) {
+                     allEnemies.splice(i, 1);
+                 }
+             }
+         });
+     }
     function update(dt) {
         updateEntities(dt);
-        // checkCollisions();
+        checkCollisions();
     }
 
     /* This is called by the update function and loops through all of the
@@ -117,7 +127,7 @@ var Engine = (function(global) {
             numRows = 6,
             numCols = 5,
             row, col;
-        
+
         // Before drawing, clear existing canvas
         ctx.clearRect(0,0,canvas.width,canvas.height)
 
